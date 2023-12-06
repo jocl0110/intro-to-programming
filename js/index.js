292db1aeaa80e4636e86e38ae6f6354f8d8a6394
Jose Luis Izquierdo Hernandez <110314046+jocl0110@users.noreply.github.com>
2023-12-05 19:34:22 -0600

    Change the text in the footer

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ const skillsList = skillsSection.querySelector('ul');
 
 
 
-copyright.innerHTML = `Jose Izquierdo ${thisYear}`;
+copyright.innerHTML = `©Jose Izquierdo ${thisYear}`;
 footer.appendChild(copyright);
 
 for(let i = 0; i < skills.length; i++){
@@ -17,3 +17,30 @@ for(let i = 0; i < skills.length; i++){
     skill.textContent = skills[i];
     skillsList.appendChild(skill);
 }
+
+// It didn't let me use the document.getElementsByName method in the line #22. 
+const messageForm = document.querySelector('form');
+messageForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const usersName = e.target.usersName.value;
+    const usersEmail = e.target.usersEmail.value;
+    const usersMessage = e.target.usersMessage.value;
+    console.log(usersName)
+    console.log(usersEmail)
+    console.log(usersMessage)
+    const messageSection = document.getElementById('messages');
+    const messageList = messageSection.querySelector('ul');
+    const newMessage = document.createElement('li');
+    newMessage.innerHTML = `<a href="mailto:${usersEmail}">${usersName} </a><span>wrote you ${usersMessage}</span>`;
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'remove';
+    removeButton.type = "button";
+    removeButton.addEventListener('click', (e) => {
+        const entry = removeButton.parentNode;
+        entry.remove();
+
+    })
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+    messageForm.reset();
+})
